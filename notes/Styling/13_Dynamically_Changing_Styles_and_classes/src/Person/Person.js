3681c80733f415a3899b3f622ef7e52fa39088cf
Hartz-I <ahnafsadikinan@gmail.com>
2020-11-14 16:22:13 +0600

    Made some adjustments and styles in react

---
+++ notes/Styling/13_Dynamically_Changing_Styles_and_classes/src/Person/Person.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Person.css"; //add css
+
+const person = (props) => {
+  //we are using function to create component not class
+  return (
+    //use defaultValue to avoid default errors
+    <div className="Person">
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} defaultValue={props.name} />
+    </div>
+    //we use {} to use js in jsx
+    //props are used to add properies when the function is called
+    //children takes anything between the opening and closing tag
+  );
+};
+
+export default person;
